a3b1d5d542376ebb5871047d54ae4e817618ece1
isheikh8492 <60596395+isheikh8492@users.noreply.github.com>
2023-11-09 16:07:25 -0600

    added blank drawer toggle

--- app/src/main/java/com/fetchrewards/codingexercise/MainActivity.java
+++ app/src/main/java/com/fetchrewards/codingexercise/MainActivity.java
@@ -1,12 +1,16 @@
 package com.fetchrewards.codingexercise;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -17,9 +21,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -36,13 +42,33 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView errorTxtView;
 
-
     private LinearLayoutManager linearLayoutManager;
+
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerList = findViewById(R.id.drawer_list);
+
+        drawerList.setOnItemClickListener(
+                (parent, view, position, id) -> {
+                    selectItem(position);
+                    drawerLayout.closeDrawer(drawerList);
+                }
+        );
+
+        drawerToggle = new ActionBarDrawerToggle(
+                this,            /* host Activity */
+                drawerLayout,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        );
 
         recyclerView = findViewById(R.id.recycler);
         itemAdapter = new ItemAdapter(itemList, this);
@@ -52,11 +78,32 @@ public class MainActivity extends AppCompatActivity {
         errorTxtView = findViewById(R.id.error);
         errorTxtView.setVisibility(View.GONE);
 
-        // Updated to include border width and corner radius
         BorderItemDecoration borderItemDecoration = new BorderItemDecoration(this);
         recyclerView.addItemDecoration(borderItemDecoration);
 
         doDownload();
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void selectItem(int position) {
+        Log.d(TAG, "selectItem: " + position);
     }
 
     @Override
@@ -66,6 +113,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            Log.d(TAG, "onOptionsItemSelected: mDrawerToggle " + item);
+            return true;
+        }
 
         if (item.getItemId() == R.id.filterByName) {
             if (this.itemListWithNameOnly.isEmpty()) {
